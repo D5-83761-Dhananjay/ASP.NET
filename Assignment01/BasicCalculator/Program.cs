2eb3bc9649edcb360332c50d10395af99302e513
dgnagargoje11 <dgnagargoje11@gmail.com>
2024-05-16 15:54:15 +0530

    Q1 submitted

---
+++ Assignment01/BasicCalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter x ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1: add 2: sub 3: mul 4:div");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calculator = new Calculator();
+            switch (choice)
+            {
+                case 1: Console.WriteLine("addition ="+calculator.add(num1, num2));
+                    Console.ReadLine(); break;
+                case 2: Console.WriteLine("subtraction =" + calculator.sub(num1, num2)); Console.ReadLine();  break;
+                case 3: Console.WriteLine("multi ="+calculator.mul(num1, num2)); Console.ReadLine();  break;
+                case 4: Console.WriteLine("division ="+calculator.div(num1, num2)); Console.ReadLine();  break;
+            }
+        }
+        private class Calculator {
+            public int add(int x, int y)
+            {
+                return x + y;
+            }
+            public int sub(int x, int y)
+            {
+                return x - y;
+            }
+            public int mul(int x, int y)
+            {
+                return x * y;
+            }
+            public int div(int x, int y)
+            {
+                return x / y;
+            }
+
+        }
+    }
+}
---
+++ Assignment01/CalUsingDowhile/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalUsingDowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Calculator calculator = new Calculator();
+            calculator.menu();
+        }
+
+        private class Calculator
+        {
+            private int choice;
+            public void add()
+            {
+                Console.WriteLine("enter x ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter y");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int a = num1 + num2;
+                Console.WriteLine(a);
+
+            }
+            public void sub()
+            {
+                Console.WriteLine("enter x ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter y");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int a = num1 - num2;
+                Console.WriteLine(a);
+            }
+            public void mul()
+            {
+                Console.WriteLine("enter x ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter y");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int a = num1 * num2;
+                Console.WriteLine(a);
+            }
+            public void div()
+            {
+                Console.WriteLine("enter x ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter y");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int a = num1 / num2;
+                Console.WriteLine(a);
+            }
+
+            public void menu()
+            {
+                do
+                {
+                    Console.WriteLine("0: exit 1: add 2: sub 3: mul 4:div");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 1)
+                    {
+                        add();
+                    }
+                    else if(choice == 2)
+                    {
+                        sub();
+                    }
+                    else if (choice == 3)
+                    {
+                        mul();
+                    }
+                    else if (choice == 4)
+                    {
+                        div();
+                    }
+                    else
+                    {
+                        Console.WriteLine("enter valid choice");
+                        Console.ReadLine();
+                    }
+                } while (choice!=0);
+            }
+
+        }
+
+    }
+}
